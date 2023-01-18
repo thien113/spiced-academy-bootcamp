368d2c491cf61feba27bf06be33ec2d9055c944e
thien113 <n-tv@hotmail.de>
2023-01-18 10:44:16 +0100

    standard js declaration

---
+++ js/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,33 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const postTitle = "My Self-Made Candy";
+const postText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+let numberOfPostLikes = 500;
+const userId = 1253;
+let isReported = false;
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log("The Posttitle is: " + postTitle);
+console.log("The Posttext is: " + postText);
+console.log("The number of likes are: " + numberOfPostLikes);
+console.log(numberOfPostLikes + 28 + " New Likecounter!");
+console.log("It's reported? " + isReported);
+
+// --^-- write your code here --^--
