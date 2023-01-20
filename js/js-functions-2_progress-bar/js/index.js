c355af8c918410e7d5d8817b93a8161ebafdff94
thien113 <n-tv@hotmail.de>
2023-01-20 16:21:21 +0100

    progress bar challenge

--- js/js-functions-2_progress-bar/js/index.js
+++ js/js-functions-2_progress-bar/js/index.js
@@ -20,5 +20,19 @@ For that:
 */
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
+addEventListener("scroll", () => {
+  let percent = calculateScrollPercentage();
+  let newPercent = percent.toString();
+  progressBar.style.width = percent + "%";
+});
 
-function calculateScrollPercentage() {}
+function calculateScrollPercentage() {
+  var h = document.documentElement,
+    b = document.body,
+    st = "scrollTop",
+    sh = "scrollHeight";
+  result = Math.round(
+    ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100
+  );
+  return result;
+}
