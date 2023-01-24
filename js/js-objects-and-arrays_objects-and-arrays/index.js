f9f55bc449db09ad341fe954a5f03aec61465648
thien113 <n-tv@hotmail.de>
2023-01-24 12:18:55 +0100

    objects-arrays: object&arrays challenge done

--- js/js-objects-and-arrays_objects-and-arrays/index.js
+++ js/js-objects-and-arrays_objects-and-arrays/index.js
@@ -50,27 +50,35 @@ const people = [
 // EXERCISE 1
 // Change the value of the variable nameOfFirstPerson to equal the name of the first person.
 
-const nameOfFirstPerson = "change me";
+const nameOfFirstPerson = people[0].name;
 
 // EXERCISE 2
 // Change the value of the variable homersOtherFriends to equal the other friends of Homer.
 
-const homersOtherFriends = "change me";
+const homersOtherFriends = people[0]["other friends"];
 
 // EXERCISE 3
 // Change the value of the variable ageOfThirdPerson to equal the age of the third person.
 
-const ageOfThirdPerson = "change me";
+const ageOfThirdPerson = people[2].age;
 
 // EXERCISE 4
 // Change the value of the variable mothersNameOfSecondPerson to equal the mothers name of the second person.
 
-const mothersNameOfSecondPerson = "change me";
+const mothersNameOfSecondPerson = people[1].parents.mothersName;
 
 // EXERCISE 5
 // Change the value of the variable animaltypeOfLisasPet to equal the type of animal of Lisas pet.
 
-const animaltypeOfLisasPet = "change me";
+const objLisa = people.map((obj) => {
+  if (obj.name === "Lisa") {
+    const animaltypeOfLisasPet = objLisa.pet.animal;
+    console.log(animaltypeOfLisasPet);
+    return obj;
+  } else {
+    return obj;
+  }
+});
 
 export {
   nameOfFirstPerson,
