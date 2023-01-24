c9dabb95d798bb75ea742627ae54d50ec56bd8c8
thien113 <n-tv@hotmail.de>
2023-01-24 10:58:44 +0100

    objects-arrays: object challenge done

--- js/js-objects-and-arrays_objects/index.js
+++ js/js-objects-and-arrays_objects/index.js
@@ -4,7 +4,9 @@ console.clear();
 // Create an object personWithNameAgeEmail with the properties "name", "age" and "email".
 
 const personWithNameAgeEmail = {
-  example: "example",
+  name: "John Dough",
+  age: "21",
+  email: "johndough@donut.com",
 };
 
 // EXERCISE 2
@@ -15,8 +17,8 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex.age;
 
 // EXERCISE 3
 // Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.
@@ -25,6 +27,8 @@ const personToChange = {
   name: "Kim",
   age: 24,
 };
+personToChange.name = "Alex";
+personToChange.age = 35;
 
 // EXERCISE 4
 // Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
@@ -33,6 +37,8 @@ const petPluto = {
   name: "Pluto",
   species: "dog",
 };
+petPluto.age = 5;
+petPluto.breed = "husky";
 
 export {
   personWithNameAgeEmail,
