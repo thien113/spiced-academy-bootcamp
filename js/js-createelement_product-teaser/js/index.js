f8c799d32114acb899fd8d6d3203ef1ca7eed222
thien113 <n-tv@hotmail.de>
2023-01-25 11:55:05 +0100

    shop -> create elements with innerhtml and parse the data

--- js/js-createelement_product-teaser/js/index.js
+++ js/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,34 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+//create new article
+const newArticle = document.createElement("article");
+newArticle.classList.add("product");
+
+newArticle.innerHTML = `
+      <div class="product__body">
+        <div class="product__text-container">
+          <h2 class="product__name">${name}</h2>
+          <ul class="product__categories">
+            <li class="product__category">${category1}</li>
+            <li class="product__category">${category2}</li>
+            <li class="product__category">${category3}</li>
+          </ul>
+          <p class="product__description">
+            ${description}
+          </p>
+        </div>
+        <div class="product__image-container">
+          <img
+            class="product__image"
+            src="${imageSrc}"
+            alt=""
+          />
+        </div>
+      </div>
+      <footer class="product__footer">
+        <span class="product__price">${price}</span>
+        <button type="button" class="product__buy-button">Buy</button>
+      </footer>`;
+document.body.append(newArticle);
