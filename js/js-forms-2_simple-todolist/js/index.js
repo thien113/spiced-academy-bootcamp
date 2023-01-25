fbdf1b05b7066379f5b3288d199b3b4b171211f0
thien113 <n-tv@hotmail.de>
2023-01-25 16:43:30 +0100

    js forms: todo list with event.target / object.fromentries()

--- js/js-forms-2_simple-todolist/js/index.js
+++ js/js-forms-2_simple-todolist/js/index.js
@@ -2,3 +2,30 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const todoList = document.querySelector('[data-js="todoList"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // gives whole Data
+  const formData = new FormData(event.target);
+
+  //gives objects with attribute
+  const formDataAsObjects = Object.fromEntries(formData);
+
+  //create li element
+  const listItem = document.createElement("li");
+
+  // getvalues of headline and task
+  const headline = formDataAsObjects.headline;
+  const task = formDataAsObjects.task;
+
+  //parse value
+  listItem.textContent = `Headline: ${headline} Task: ${task}`;
+
+  //appending logic
+  todoList.append(listItem);
+
+  // after submitting logic
+  event.target.reset();
+  event.target.elements.headline.focus();
+});
