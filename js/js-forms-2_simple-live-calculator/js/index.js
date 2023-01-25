d48841739c44e60430746191d2a525fe887cb694
thien113 <n-tv@hotmail.de>
2023-01-25 16:03:30 +0100

    js forms: live calculator with eventhandler function

--- js/js-forms-2_simple-live-calculator/js/index.js
+++ js/js-forms-2_simple-live-calculator/js/index.js
@@ -4,9 +4,18 @@ const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
 
+// every addEventListener execute a function
+//either a before-defined function
+
+//arrow function
 const eventHandler = () => {
   result.textContent = firstInput.value * secondInput.value;
 };
-
+// or a function that will directly implemented inside it
 firstInput.addEventListener("input", eventHandler);
 secondInput.addEventListener("input", eventHandler);
+
+/*
+In any case: eventlistener has 2 parameters, an event trigger eg input
+and a function (before-defined or on the getgo)
+*/
