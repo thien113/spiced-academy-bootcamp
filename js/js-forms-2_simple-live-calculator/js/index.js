d41dc9ab5fb2e5daf640a842a1c058256a94861c
thien113 <n-tv@hotmail.de>
2023-01-25 16:00:24 +0100

    js forms: live calculator with eventhandler function

--- js/js-forms-2_simple-live-calculator/js/index.js
+++ js/js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,10 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+const eventHandler = () => {
+  result.textContent = firstInput.value * secondInput.value;
+};
+
+firstInput.addEventListener("input", eventHandler);
+secondInput.addEventListener("input", eventHandler);
