f77e696a5d48ce4d4882f50aa13b1f2c2f2f11cf
thien113 <n-tv@hotmail.de>
2023-01-26 11:12:33 +0100

    toogle answer

--- recap-project-1/index.js
+++ recap-project-1/index.js
@@ -8,3 +8,19 @@ function onChangeImage(objImg) {
     objImg.setAttribute("alt", "not bookmarked");
   }
 }
+
+// toggle Answer
+function toggleAnswer(objButton) {
+  const spanElement = objButton.nextElementSibling;
+  spanElement.classList.toggle("hidden");
+  toggleAnswerText(objButton);
+}
+
+function toggleAnswerText(objButton) {
+  const spanElement = objButton.nextElementSibling;
+  if (spanElement.classList.value != "hidden") {
+    objButton.textContent = "Hide Answer";
+  } else {
+    objButton.textContent = " Show Answer";
+  }
+}
