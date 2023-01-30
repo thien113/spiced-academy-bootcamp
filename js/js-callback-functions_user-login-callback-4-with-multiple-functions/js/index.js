f1628af6e8980fc6dd6cec4da1e3cd9378e36c80
thien113 <n-tv@hotmail.de>
2023-01-30 11:03:47 +0100

    basic callback function with with multiple parameter

--- js/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ js/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -9,3 +9,12 @@ function handleUserLogin(onSuccess, onError, userName) {
 }
 
 // The exercise starts here!
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+function showErrorMessage(errorMessage) {
+  console.log(`Login error: ${errorMessage}`);
+}
+handleUserLogin(showWelcomeMessage, showErrorMessage, "Jane Doe");
+handleUserLogin(showWelcomeMessage, showErrorMessage, "John Doe");
