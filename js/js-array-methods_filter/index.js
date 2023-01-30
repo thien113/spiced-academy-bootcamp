87a5232271ee7013dd6f74f2eaa9f520ce4b228a
thien113 <n-tv@hotmail.de>
2023-01-30 16:05:10 +0100

    filter array method

--- js/js-array-methods_filter/index.js
+++ js/js-array-methods_filter/index.js
@@ -23,13 +23,16 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => card === 2);
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter((card) => card.tags.length > 3);
 
-const allCardsThatAreNotBookmarked = null;
-
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter(
+  (card) => card.isBookmarked === true
+);
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter(
+  (card) => card.tags.includes("html") || card.tags.includes("js")
+);
 
 export {
   onlyCardWithIdTwo,
