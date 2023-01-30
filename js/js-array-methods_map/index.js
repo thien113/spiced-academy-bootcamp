d2611c4c805672c74407e090b855abb9f6c1f904
thien113 <n-tv@hotmail.de>
2023-01-30 16:49:05 +0100

    map array method returning obj and strings

--- js/js-array-methods_map/index.js
+++ js/js-array-methods_map/index.js
@@ -24,10 +24,14 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => card.answer.toLowerCase()); // ['as often as you like.', ...]
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map(
+  (card) => card.question + card.answer
+); // ["How often can I use <header>? - As often as you like.", ...]
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionAndAnswer = cards.map((card) => {
+  `question: ${card.question}, answer: ${card.answer}`;
+}); // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
