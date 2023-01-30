b250135d5e0e019ba57352b1cee2bab5e3a15fd6
thien113 <n-tv@hotmail.de>
2023-01-30 10:54:56 +0100

    basic callback function with two  parameter

--- js/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ js/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,27 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+function showWelcomeMessage(userName) {
+  console.log(`Welcome ${userName}! You are logged in now.`);
+}
+function hide(userName) {
+  console.log(`${userName} is hidden`);
+}
+handleUserLogin(showWelcomeMessage);
+handleUserLogin(hide);
+
+//handerUserLogin passes a function with a parameter as a parameter
+handleUserLogin((userName) => {
+  console.log(`Welcome ${userName}! You are logged in now(arrow function).`);
+});
+
+/*
+anonymous function is called with no name and passes userName as parameter
+just like an addvenetlistener
+handlerUserLogin(
+  function(userName) 
+)
+
+function is consolelog smth
+
+*/
