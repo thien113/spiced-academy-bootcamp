fb1541ab8b4a8a9cfc8c94eb46e3850ba5b57ff1
thien113 <n-tv@hotmail.de>
2023-01-30 15:52:06 +0100

    foreach function

--- js/js-array-methods/for-each/js/index.js
+++ js/js-array-methods/for-each/js/index.js
@@ -19,4 +19,13 @@ const colors = [
   "#A5A5A5",
 ];
 
-colors.forEach();
+colors.forEach((color) => {
+  renderColorBox(color);
+});
+
+function renderColorBox(color) {
+  const div = document.createElement("div");
+  div.classList.add("color-box");
+  div.style.backgroundColor = color;
+  document.body.append(div);
+}
