ffb2a587e5881e382852245a834b350a605e908a
thien113 <n-tv@hotmail.de>
2023-01-30 10:58:57 +0100

    basic callback function with two  parameter

--- js/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
+++ js/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -5,3 +5,12 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+handleUserLogin(showWelcomeMessage);
+handleUserLogin((userName, userRole) => {
+  console.log(
+    `Welcome ${userName}! You are logged in now as ${userRole}. arrow function`
+  );
+});
