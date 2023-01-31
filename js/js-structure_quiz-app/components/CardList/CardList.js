61318b6bdedbb30fdfbdc340c44b61f078ce45aa
thien113 <n-tv@hotmail.de>
2023-01-31 16:54:52 +0100

    cards challenges

---
+++ js/js-structure_quiz-app/components/CardList/CardList.js
@@ -0,0 +1,13 @@
+import { Card } from "../Card/Card.js";
+import { cards } from "../../index.js";
+export function CardList() {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
