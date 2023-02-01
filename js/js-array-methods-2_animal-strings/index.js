6a3adc1619a1dbe3e89cb7c1aabaa7a6a98ec972
thien113 <n-tv@hotmail.de>
2023-02-01 11:09:03 +0100

    first challenge: animal strings

--- js/js-array-methods-2_animal-strings/index.js
+++ js/js-array-methods-2_animal-strings/index.js
@@ -21,15 +21,19 @@ const animalStrings = [
   "rhino",
 ];
 
-const hippoExists = null;
+const hippoExists = animalStrings.includes("hippo");
 
 // Hint: You can pass the starting index as second parameter.
-const catStartingFromIndexFiveExists = null;
+const catStartingFromIndexFiveExists = animalStrings.findIndex((animal) =>
+  animal.includes("cat")
+);
 
 // Hint: Besides the array method, check out the string method `startsWith()`.
-const firstAnimalStartingWithLetterP = null;
+const firstAnimalStartingWithLetterP = animalStrings.find((animal) =>
+  animal.startsWith("p")
+);
 
-const indexOfGiraffe = null;
+const indexOfGiraffe = animalStrings.indexOf("giraffe");
 
 // Note:
 // - Sorting strings is slightly more complicated than sorting numbers.
@@ -39,18 +43,26 @@ const indexOfGiraffe = null;
 // Hint: There is no need to upper-/lowercase the strings before sorting them.
 // Hint: sort() mutates the original array, which is bad.
 // -> Use animals.slice().sort(...) to make a copy.
-const animalsSortedAlphabetically = null;
+const animalsSortedAlphabetically = animalStrings.slice().sort((a, b) => {
+  if (a > b) {
+    return -1;
+  } else {
+    return 1;
+  }
+});
 
 // Hint: Guess what? There is a string method called `endsWith()`.
-const anyAnimalEndsWithLetterZ = null;
+const anyAnimalEndsWithLetterZ = animalStrings.endsWith("z");
 
-const everyAnimalHasMoreThanTwoLetters = null;
+const everyAnimalHasMoreThanTwoLetters = animalStrings.filter(
+  (animal) => animal.length > 2
+);
 
 // Hint: There are several ways to go here. Let's focus on two options:
 // Option 1: Concatenate all characters with `reduce()` and check for the `length` property of the result.
 // Option 2: Use `map()` to create an array with the length values of all strings,
 // 				then sum them up with `reduce()` (keyword: method chaining)
-const sumOfAllAnimalCharacters = null;
+const sumOfAllAnimalCharacters = animalStrings.reduce().length;
 
 export {
   hippoExists,
