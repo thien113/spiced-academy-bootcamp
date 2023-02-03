892581f6edbf48e40730c87432e20f64cdfd732b
thien113 <n-tv@hotmail.de>
2023-02-03 15:01:46 +0100

    js unit test challenge 1 done

--- js/js-unit-testing_get-animal/index.js
+++ js/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return "I totally love cats!";
+  } else if (animal === "dogs") {
+    return "I like dogs!";
+  } else {
+    return "I do not like animals at all!";
+  }
+}
