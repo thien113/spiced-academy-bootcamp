b4796ab573efdafdd340abd3a30b44ed673db45b
thien113 <n-tv@hotmail.de>
2023-02-03 11:35:18 +0100

    third challenge

--- js/js-modern-syntax_rest-and-spread/index.js
+++ js/js-modern-syntax_rest-and-spread/index.js
@@ -12,6 +12,7 @@ const person = {
   email: "jane@doe.com",
   phone: "12345",
 };
+export const { name, ...contact } = person;
 
 // example: export const {value 1} = myObject;
 
@@ -20,6 +21,7 @@ const person = {
 // all other items should go into a variable called "restOfList".
 
 const list = ["one", "two", "three", "four", "five"];
+export const [first, second, restOfList] = list;
 
 // EXERCISE 3
 // a) The `add` function does not work because it needs an array as argument.
@@ -27,7 +29,7 @@ const list = ["one", "two", "three", "four", "five"];
 // c) Make sure you understand what the rest syntax does here.
 // d) Note that the optional chaining `?.` is used to prevent an error to happen.
 
-function add(allNumbers) {
+function add(...allNumbers) {
   return allNumbers.reduce?.((a, b) => a + b);
 }
 
@@ -39,7 +41,7 @@ export const sum = add(3, 2, 3, 2, 1, 2, 3, 4);
 // c) Make sure you understand what the spread syntax does here.
 
 const numbers = [3, 2, 1];
-export const result = add(numbers);
+export const result = add(...numbers);
 
 // EXERCISE 5
 // a) Create a variable `allFruits` which contains `anotherFruit` and `fruits`
@@ -49,3 +51,4 @@ export const result = add(numbers);
 
 const fruits = ["apple", "banana", "orange", "papaya"];
 const anotherFruit = "cherry";
+export const allFruits = [...anotherFruit, ...fruits];
