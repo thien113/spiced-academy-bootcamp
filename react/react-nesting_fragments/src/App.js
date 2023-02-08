e061b5a9daaff106d8b254a53eba34122b6549be
thien113 <n-tv@hotmail.de>
2023-02-08 10:54:01 +0100

    react nesting 2. challenge

--- react/react-nesting_fragments/src/App.js
+++ react/react-nesting_fragments/src/App.js
@@ -5,9 +5,16 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <main className="flex-container">
+      <Boxes />
+    </main>
+  );
+}
+function Boxes() {
+  return (
+    <>
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </main>
+    </>
   );
 }
