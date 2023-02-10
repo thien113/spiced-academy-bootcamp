7f69ebd5b9530b44cee38bef3facba02ee11a24b
thien113 <n-tv@hotmail.de>
2023-02-10 16:16:24 +0100

    react state handle arrays of objects challenge 2

--- react/react-state-3_tags/src/App.js
+++ react/react-state-3_tags/src/App.js
@@ -6,10 +6,17 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+  function handleDeleteTag(tagRemove) {
+    const filteredTags = tags.filter((tag) => tag !== tagRemove);
+    setTags(filteredTags);
+  }
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
