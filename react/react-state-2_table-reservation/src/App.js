f7930b159e5dd63ce225b6bb7e8832fff7c5fd2b
thien113 <n-tv@hotmail.de>
2023-02-10 12:06:07 +0100

    react lift up state challenge 2 done

--- react/react-state-2_table-reservation/src/App.js
+++ react/react-state-2_table-reservation/src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { useState } from "react";
+
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function addPeopleHandle() {
+    setPeople(people + 1);
+  }
+  function subbPeopleHandle() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        onAddPeopleHandle={addPeopleHandle}
+        onSubbPeopleHandle={subbPeopleHandle}
+        people={people}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
--- react/react-state-2_table-reservation/src/components/Counter.js
+++ react/react-state-2_table-reservation/src/components/Counter.js
@@ -1,16 +1,18 @@
-import { useState } from "react";
-
-export default function Counter() {
-  const [people, setPeople] = useState(0);
+export default function Counter({
+  onSubbPeopleHandle,
+  onAddPeopleHandle,
+  people,
+}) {
   return (
     <>
       <h2>How many people would you like to visit us with?</h2>
+      <p>{people}</p>
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment people count"
-          onClick={() => setPeople(people + 1)}
+          onClick={onAddPeopleHandle}
         >
           +
         </button>
@@ -18,7 +20,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement people count"
-          onClick={() => setPeople(people - 1)}
+          onClick={onSubbPeopleHandle}
         >
           -
         </button>
