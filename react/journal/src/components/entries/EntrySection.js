d95390f0d09273d63b2ed0c554344414d3013906
thien113 <n-tv@hotmail.de>
2023-02-13 16:52:15 +0100

    tabs working

--- react/journal/src/components/entries/EntrySection.js
+++ react/journal/src/components/entries/EntrySection.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Tabs from "../tabs/Tabs";
 import Star from "../star/Star";
 import "./EntrySection.css";
 
 export default function EntrySection({ setMotto }) {
   const cards = JSON.parse(localStorage.getItem("cards") || "[]");
-
+  const favsCards = cards.filter((card) => card.star === true);
+  const [isClicked, setIsClicked] = useState(false);
+  console.log("isClicked:", isClicked);
   function handleChangeBoolean(id) {
     //filter by card.id of array items
     const filteredCard = cards.find((card) => card.id === id);
@@ -25,23 +28,41 @@ export default function EntrySection({ setMotto }) {
   }
   return (
     <section>
-      <Tabs />
-      {cards.map((card) => (
-        <article key={card.id}>
-          <h4>{card.date}</h4>
-          <div className="title">
-            <h2>
-              <strong>"{card.motto}"</strong>
-            </h2>
-            <Star
-              starType={card.star}
-              id={card.id}
-              handleChange={handleChangeBoolean}
-            />
-          </div>
-          <p>{card.notes}</p>
-        </article>
-      ))}
+      <Tabs setIsClicked={setIsClicked} />
+      {(isClicked === "entries" || isClicked === false) &&
+        cards.map((card) => (
+          <article key={card.id}>
+            <h4>{card.date}</h4>
+            <div className="title">
+              <h2>
+                <strong>"{card.motto}"</strong>
+              </h2>
+              <Star
+                starType={card.star}
+                id={card.id}
+                handleChange={handleChangeBoolean}
+              />
+            </div>
+            <p>{card.notes}</p>
+          </article>
+        ))}
+      {isClicked === "favs" &&
+        favsCards.map((card) => (
+          <article key={card.id}>
+            <h4>{card.date}</h4>
+            <div className="title">
+              <h2>
+                <strong>"{card.motto}"</strong>
+              </h2>
+              <Star
+                starType={card.star}
+                id={card.id}
+                handleChange={handleChangeBoolean}
+              />
+            </div>
+            <p>{card.notes}</p>
+          </article>
+        ))}
     </section>
   );
 }
--- react/journal/src/components/tabs/Tabs.js
+++ react/journal/src/components/tabs/Tabs.js
@@ -1,14 +1,16 @@
 import "./Tabs.css";
 
-export default function Tabs() {
+export default function Tabs({ setIsClicked }) {
   const cards = JSON.parse(localStorage.getItem("cards") || "[]");
-
+  function isClickedHandler(event) {
+    setIsClicked(event.target.className);
+  }
   return (
     <ul className="tabs">
-      <h3 className="entries">
+      <h3 className="entries" onClick={isClickedHandler}>
         All Entries<span className="active">{cards.length}</span>
       </h3>
-      <h3 className="favs">
+      <h3 className="favs" onClick={isClickedHandler}>
         Favorites
         <span className="disabled">
           {cards.filter((card) => card.star === true).length}
