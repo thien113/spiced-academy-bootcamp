3c562758480a09c911fae665376f1a902770f347
thien113 <n-tv@hotmail.de>
2023-02-13 17:38:22 +0100

    local storage challenges 1 done

---
+++ react/react-with-local-storage_stored-note/src/App.js
@@ -0,0 +1,27 @@
+import useLocalStorageState from "use-local-storage-state";
+
+import FontSelector from "./components/FontSelector";
+import Note from "./components/Note";
+import "./styles.css";
+
+export default function App() {
+  const [note, setNote] = useLocalStorageState("note", { defaultValue: "" });
+  const [font, setFont] = useLocalStorageState("font", {
+    defaultValue: "system-ui",
+  });
+
+  function handleNoteChange(newNote) {
+    setNote(newNote);
+  }
+
+  function handleFontChange(newFont) {
+    setFont(newFont);
+  }
+
+  return (
+    <div className="app" style={{ "--font": font }}>
+      <FontSelector font={font} onFontChange={handleFontChange} />
+      <Note note={note} onNoteChange={handleNoteChange} />
+    </div>
+  );
+}
