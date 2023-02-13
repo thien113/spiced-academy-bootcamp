ee9429e81449e348dc79e06094d3fac522192f7f
thien113 <n-tv@hotmail.de>
2023-02-13 16:04:11 +0100

    journal app has now a toggle svg feature

--- react/journal/src/components/star/Star.js
+++ react/journal/src/components/star/Star.js
@@ -2,8 +2,7 @@ import { ReactComponent as StarEmpty } from "./star.svg";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 
 export default function Star({ starType, id, handleChange }) {
-  console.log("star component all values:", starType);
-  if (!{ starType }) {
+  if (!starType) {
     return (
       <StarEmpty
         onClick={() => {
