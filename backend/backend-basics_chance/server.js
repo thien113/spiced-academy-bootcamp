6fbaa4a7cd18e16dd23e323813bf955a5f253b8a
thien113 <n-tv@hotmail.de>
2023-02-23 11:45:05 +0100

    challenge 3

--- backend/backend-basics_chance/server.js
+++ backend/backend-basics_chance/server.js
@@ -1,3 +1,18 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    const name = chance.name();
+    const age = chance.age({ type: "child" });
+    const profession = chance.profession();
+    response.statusCode = 200;
+    response.end(
+      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    );
+  } else {
+    response.statusCode = 404;
+    response.end("Not Found");
+  }
+});
