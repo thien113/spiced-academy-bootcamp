6df33329925c6c57dce7a13187123250e968d175
thien113 <n-tv@hotmail.de>
2023-02-23 11:25:02 +0100

    test pass everything

--- backend/backend-basics_greeting/server.js
+++ backend/backend-basics_greeting/server.js
@@ -1,9 +1,8 @@
 import { createServer } from "node:http";
 
 export const server = createServer((request, response) => {
-  const name = "Thien";
   if (request.url === "/") {
     response.statusCode = 200;
-    response.end(`Hello ${name}`);
+    response.end("Hello, Thien");
   }
 });
