540d0e98945463ab6bfc5d541e6eff2c4f6af165
thien113 <n-tv@hotmail.de>
2023-02-23 11:22:48 +0100

    challenge 2 done

--- backend/backend-basics_routes/index.js
+++ backend/backend-basics_routes/index.js
@@ -1,3 +1,6 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`);
+});
--- backend/backend-basics_routes/server.js
+++ backend/backend-basics_routes/server.js
@@ -1,3 +1,14 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  if (request.url === "/api/fish/1") {
+    response.statusCode = 200;
+    response.end("Shrimp");
+  } else if (request.url === "/api/fish/2") {
+    response.statusCode = 200;
+    response.end("Anemonefish");
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
+  }
+});
